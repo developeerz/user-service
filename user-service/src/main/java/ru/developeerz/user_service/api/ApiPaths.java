7e18e9dc898515bdfb1fbdeb3da7712f2e7f4f2c
mikhail <mihaelvozhzhov@gmail.com>
2025-03-10 12:10:25 +0700

    TASK-2 тест на работоспособность

---
+++ user-service/src/main/java/ru/developeerz/user_service/api/ApiPaths.java
@@ -0,0 +1,10 @@
+package ru.developeerz.user_service.api;
+
+public class ApiPaths {
+
+    public static final String REGISTRATION = "/user/registration";
+
+    public static final String LOGIN = "/user/login";
+
+    public static final String VERIFY = "/user/verify";
+}
